d655403cf4f319d85151cd188692e3f8411caf03
brukGit <125497921+brukGit@users.noreply.github.com>
2023-03-26 16:54:12 +0300

    Effective to-do list controller using event delegation.
    Event delegation was used -
    instead of attaching events to individual imgs or divs, delegating it to their parent li helped in effective control on which lists to remove and which lists to keep.
    
    A sample event bubbling control mechanism was also implemented using one of the element images to interfer with the delegated event to the parent list.

--- script.js
+++ script.js
@@ -28,60 +28,83 @@
  }
 displayDate();
 
- 
- // array containing IDs.
-//  var idNames = ['prio1','prio1bul','prio2','prio2bul',
-// 'prio3','prio3bul','prio4','prio4bul','prio5','prio5bul']
 
- var elReport = document.getElementById('reportlimit');
- function removeTask(event) {
-    // console.log(event)
-    // console.log(event.target)
-    event.target.parentElement.remove()
+// let elRemoveImg =  document.querySelectorAll('img.delete_task');
+ let elReport = document.getElementById('reportlimit');
+ let elRemoveImg =  document.querySelectorAll('div.delImgContainer');
+ function removeTask(event) {   
     
+    // update list index - used for identifying main and sub task lists.
+    index -= event.target.parentElement.parentElement.children.length
+  
+    event.target.parentElement.parentElement.remove()    
        
     }
 
 // Define function that looks for user inputs
+
+
 function addTask(){
-    let elTask = document.querySelector('ol.task-main')
+    elTask = document.querySelector('ol.task-main')
     let elList = document.createElement('li')
 
     if(index%2!=0){
         let elUlist = document.createElement('ul')
         elList.innerText = elUI.value
+        elUlist.setAttribute('class','subTask')
         elUlist.append(elList)
        // bullet poins categroy
         let elsubtasks = document.querySelectorAll('li')
-        console.log(elsubtasks)
+        // console.log(elsubtasks)
         elsubtasks.item(index-1).append(elUlist)
+
+        elUI.placeholder = 'New task title..'
 
     }
     else {
-        elList.innerText = elUI.value
-        elTask.append(elList)
-        console.log(elTask)
+        // let textDiv = document.createElement('em')
+        // textDiv.innerText = elUI.value
+        // elList.append(textDiv)
+        elList.innerText = elUI.value 
+        elList.setAttribute('class','taskTitle') 
 
+        // elList.classList.add('')     
+        let imgDiv = document.createElement('div')
+        imgDiv.setAttribute('class','delImgContainer')
         let elImg = document.createElement('img')
         elImg.src = "images/trash_can.png"
-        elImg.setAttribute('class', 'delete_task')
-        elTask.append(elImg)
+        elImg.setAttribute('class', 'deleteTask')
+        
 
-        elRemoveImg = document.querySelectorAll('img.delete_task')
-        elRemoveImg.forEach((remove) => {
-            remove.addEventListener('click',(event) => {
-                // event.stopPropagation()
-                // console.log('img event triggered.')
-                removeTask(event)
+        imgDiv.append(elImg)
+        elList.append(imgDiv)
+        elTask.append(elList)
+
+        // This code snippet below is used only to see event.stopPropagation()
+        //  functionality to stop bubbling. 
+        elImgs = document.querySelectorAll('img.deleteTask')
+        if(elImgs.length == 3){
+            // if there is no image for testing bubbline, 
+            //  then make one.
+            if(document.querySelectorAll('.stopprop').length <1){
                 
-                
-            })
-        })
-        // console.log(elRemoveImg)
+                elImg.classList.add('stopprop')
+                let imgStopProp = document.querySelector('.stopprop')
+                imgStopProp.addEventListener('click', (event) => {
+                event.stopPropagation()
+                console.log('Not going to delete.')
+                }
+                )
+            }          
+        
+        }
+        elUI.placeholder = 'Add bullet points..'
     }
-    index++;
-    // elUI.value = '';
-    
+    index++;  
+    elUI.value = ''
+    //refocus on textArea
+    setup()
+   
 }
 
 function setup(){
@@ -92,8 +115,12 @@ window.addEventListener('load',setup(),false)
 
 //add event to save button
 
-var index = 0;
-var elSave = document.getElementById('saveTasks');
+let index = 0;
+let n_times = 0;
+
+let elTask = document.querySelector('ol.task-main')
+let elImgs = document.querySelectorAll('img')
+let elSave = document.getElementById('saveTasks');
 elSave.addEventListener('click', (event) => {
     if (elUI.value.length > 0){
         
@@ -103,12 +130,21 @@ elSave.addEventListener('click', (event) => {
     
 }, false)
 
+// Delegate task to ol - main container for task lists. 
+elTask.addEventListener('click',(event)=> {
+    // see if event comes from 'delete' img.
+    if(event.target.tagName === 'IMG'){
+        removeTask(event)
+    }
+})
 
 
 
 
 
-// Deleting task lists.
-let elRemoveImg = document.querySelectorAll('img.delete_task')
+
+
+
+
 
 
