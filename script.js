f1db04fc96548c8d6a312f1a2c84687ec2fe3427
brukGit <125497921+brukGit@users.noreply.github.com>
2023-03-28 13:59:11 +0300

    data store and load functionality added using localStorage.
    Now the app saves data to local storage space and can load it back once the page is unloaded or closed and later loaded.
    
    Basically -  it saves modified data.
    Next - Refactoring the functions and codes.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//  import {load_data} from '/loading.js'
+ 
  // Get date
  today = new Date();
  elDate = document.getElementById('date');
@@ -26,9 +28,52 @@
 displayDate();
 
 
-// let elRemoveImg =  document.querySelectorAll('img.delete_task');
- let elReport = document.getElementById('reportlimit');
- let elRemoveImg =  document.querySelectorAll('div.delImgContainer');
+// ------------------ GLOBAL VARIABLES --------------------------------- //
+// HTML components
+let elReport = document.getElementById('reportlimit');
+let elRemoveImg =  document.querySelectorAll('div.delImgContainer');
+let elUI = document.getElementById('tasksUI');
+let elTask = document.querySelector('ol.task-main')
+let elImgs = document.querySelectorAll('img')
+let elSave = document.getElementById('saveTasks');
+
+// Variables(index counters) for addTask functionalities
+let index = 0;
+let n_times = 0;
+// Data store and load variables
+let loaddata = false;
+let index_loaddata = 0;
+let taskDataContainer = []
+
+
+// ------------------    EVENTS             -----------------  //
+// focus on text area when page loads.
+window.addEventListener('load',setup(),false)
+
+elSave.addEventListener('click', (event) => {
+    if (elUI.value.length > 0){        
+        addTask()       
+    }
+    
+}, false)
+
+// Delegate task to ol - main container for task lists. 
+elTask.addEventListener('click',(event)=> {
+    // see if event comes from 'delete' img.
+    if(event.target.tagName === 'IMG'){
+        removeTask(event)
+    }
+})
+
+// Window loading setup
+function setup(){
+    
+    // load previous task list from local storage
+    tasksFromLocalStorage()
+    // focus to textArea
+    elUI.focus();            
+}
+//  ------------------ Function that removes tasks. ----------------- //
  function removeTask(event) {   
     
     // update list index - used for identifying main and sub task lists.
@@ -38,13 +83,16 @@ displayDate();
        
     }
 
-// Define function that looks for user inputs
 
+// ----------------  Function that adds task lists  ----------------------- //
 
 function addTask(){
     elTask = document.querySelector('ol.task-main')
     
-
+    if(loaddata){
+        index = index_loaddata;
+        console.log(index)
+    }
     if(index%2!=0){
         let elUlist = document.createElement('ul')
        
@@ -58,7 +106,7 @@ function addTask(){
                 let elList = document.createElement('li')
                 elList.append(items[i])
                 elUlist.append(elList)
-                console.log('I\'m in newline check')
+               
             }        
         }
         // elList.innerHTML = html;
@@ -68,7 +116,6 @@ function addTask(){
         
        // bullet poins categroy
         let elsubtasks = document.querySelectorAll('li.taskTitle')
-        console.log(elsubtasks)
         elsubtasks[(index-1)/2].append(elUlist)
 
         elUI.placeholder = 'New task title..'
@@ -95,8 +142,8 @@ function addTask(){
         // This code snippet below is used only to see event.stopPropagation()
         //  functionality to stop bubbling. 
         elImgs = document.querySelectorAll('img.deleteTask')
-        if(elImgs.length == 3){
-            // if there is no image for testing bubbline, 
+        if(elImgs.length == 2){
+            // if there is no image for testing bubblindg, 
             //  then make one.
             if(document.querySelectorAll('.stopprop').length <1){
                 
@@ -104,7 +151,11 @@ function addTask(){
                 let imgStopProp = document.querySelector('.stopprop')
                 imgStopProp.addEventListener('click', (event) => {
                 event.stopPropagation()
-                console.log('Not going to delete.')
+               
+                let data_to_store = store_data()
+                
+                localStorage.setItem('tasks',data_to_store)
+                               
                 }
                 )
             }          
@@ -112,60 +163,73 @@ function addTask(){
         }
         elUI.placeholder = 'Add bullet points..'
     }
+  
     index++;  
     elUI.value = ''
-    //refocus on textArea
-    setup()
-       
-}
-
-function setup(){
-    elUI.focus();
-}
-
-let elUI = document.getElementById('tasksUI');
-// focus on text area when page loads.
-window.addEventListener('load',setup(),false)
-
-
-let index = 0;
-let n_times = 0;
-
-let elTask = document.querySelector('ol.task-main')
-let elImgs = document.querySelectorAll('img')
-let elSave = document.getElementById('saveTasks');
-elSave.addEventListener('click', (event) => {
-    if (elUI.value.length > 0){
+    elUI.focus()
         
-        addTask()
-        // console.log(event.target)
-       
+}
+
+
+// ------------------------- fcn that loads task lists from local storage . ------------- //
+function tasksFromLocalStorage(){
+    let data_to_load = JSON.parse(localStorage.getItem('tasks'))    
+    let taskName = ''
+    
+    if(data_to_load){
+        // console.log(data_to_load.length)
+        loaddata = true;
+        data_to_load.forEach((data, outer_index) => {
+            if (outer_index%2 == 0){
+                taskName = data.title
+            }
+            else {
+                let substring = ''
+                data.subtask.forEach((data) => {
+                    substring += data + '\n';
+                }  )             
+                    
+                taskName = substring;
+            }
+            elUI.value = taskName
+            index_loaddata = outer_index;
+            
+            addTask()
+        
+        })
+            loaddata = false
     }
     
-}, false)
-
-// Delegate task to ol - main container for task lists. 
-elTask.addEventListener('click',(event)=> {
-    // see if event comes from 'delete' img.
-    if(event.target.tagName === 'IMG'){
-        removeTask(event)
-    }
-})
-
-
-
-// elTaskContainer.addEventListener('')
-
- 
+}
+        
+// ---------------- fcn that stores data on the ocal storage  ------------------------  //
+function store_data(){
+    
+    let allTasks = document.querySelector('ol')
+    taskDataContainer = []
+    
+    // collect data and store in local storage
+    
+    for(let i=0; i<allTasks.children.length; i++){
+    
+        let data = allTasks.children.item(i).innerText      
+        let dataFrmtd = data.split('\n')
+        // console.log(dataFrmtd, dataFrmtd.length)
+        let subTasks = []
+        let taskTitles = []
+        dataFrmtd.forEach((data,index) => {
+            if(index>0){
+                subTasks.push(data)
+            }
+            else{
+                taskTitles.push(data);              
+            }
+        })
+        
+        taskDataContainer.push({'title':taskTitles})
+        taskDataContainer.push({'subtask':subTasks})  
+        
+        }
+        return JSON.stringify(taskDataContainer)
+}
        
-
-
-
-
-
-
-
-
-
-
-
